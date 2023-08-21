425e6ed339a5068ff5f16041ea05c5629925a2ef
nishineko <nishi.kazu2488@gmail.com>
2023-08-22 07:28:04 +0900

    https://zenn.dev/angelica/books/52be1e365c61ea/viewer/2e7c96　のだい０４が終わった。Springbootのバージョンが３以上だとjavaxが動作しないため、jakartaに変更。解決

---
+++ chatapp/src/main/java/com/example/shatapp/application/form/CommentForm.java
@@ -0,0 +1,69 @@
+package com.example.shatapp.application.form;
+
+import lombok.Data;//getter setterのやつ
+import org.hibernate.validator.constraints.Length;//文字列の長さバリデーション：入力された文字列の長さが指定された範囲内にあるかをチェック。
+import org.springframework.lang.Nullable;//メソッドの引数や戻り値がＮｕｌｌだった場合許容するため。
+
+//import javax.validation.constraints.Email;//文字列が正しいメールアドレスのフォーマット（書式）であるかを確認する。　有効なメアドかもチェックする。
+import jakarta.validation.constraints.Email;
+//import javax.validation.constraints.NotNull;//フィールドや引数Nullチェック。
+import jakarta.validation.constraints.NotNull;
+//public @interface ExtendedEmailValidatorに使うやつ。
+import javax.validation.constraints.Pattern;
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import javax.validation.Constraint;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+import javax.validation.Payload;
+import javax.validation.ReportAsSingleViolation;
+
+//バリデーションロジックに必要なクラス。
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+@Data
+public class CommentForm {
+	@Nullable
+	@Length(max=20)
+	private String name;
+	
+	//@ExtendedEmailValidator jakartaにしたら＠Email動作したのでよい。
+	@Nullable
+	@Email
+	private String mailAddress;
+	
+	@NotNull
+	@Length(min=1, max=400)
+	private String comment;
+	
+	/*@Emailが機能しないので正規表現を定義した　@ExtendedEmailValidatorを定義する　参考　https://stackoverflow.com/questions/50535214/javax-validation-constraints-email-matching-invalid-email-address*/
+	
+	@Email(message="Please provide a valid email address")
+	@Pattern(regexp=".+@.+\\..+", message="Please provide a valid email address")
+	@Target({ElementType.FIELD, ElementType.METHOD, ElementType.ANNOTATION_TYPE})
+	@Retention(RetentionPolicy.RUNTIME)//実行時にVMに読み込まれる https://qiita.com/kashira2339/items/6450714e42c37b441514
+	@Constraint(validatedBy = ExtendedEmailValidatorImpl.class)
+	@Documented
+	@ReportAsSingleViolation
+	public @interface ExtendedEmailValidator{
+		String message() default "Please provide a valid email address";
+		Class<?>[] groups() default {};
+		Class<? extends Payload>[] payload() default {};
+	}
+	public class ExtendedEmailValidatorImpl implements ConstraintValidator<ExtendedEmailValidator, String> {
+
+	    @Override
+	    public void initialize(ExtendedEmailValidator constraintAnnotation) {
+	        // 初期化処理
+	    	
+	    }
+
+	    @Override
+	    public boolean isValid(String value, ConstraintValidatorContext context) {
+	        // バリデーションロジックを実装
+	        // 正しいメールアドレスの形式かどうかをチェックし、結果を返す
+	        return value != null && value.matches(".+@.+\\..+");
+	    }
+	}
+}
---
+++ chatapp/src/main/java/com/example/shatapp/presentation/BoardController.java
@@ -0,0 +1,33 @@
+package com.example.shatapp.presentation;
+
+import com.example.shatapp.application.form.CommentForm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;				//HTML(View層)にデータを渡す場合、利用するクラス
+
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+@Controller
+@RequiredArgsConstructor
+public class BoardController {
+	
+	@GetMapping("/board")
+	public ModelAndView viewBord(ModelAndView modelAndView) {	//HTML(View層)にデータを渡す場合、利用するクラス
+		modelAndView.setViewName("board");//	HTMLfileのパス設定を行う。
+		modelAndView.addObject("commentForm",new CommentForm());//addObjectはThymeleafにデータを渡している。
+		return modelAndView;
+	}
+	//ポストを受け取った時にバリデーションを動作させる。
+	@PostMapping("/board")
+	public ModelAndView PostComment(@Validated @ModelAttribute CommentForm comment,BindingResult bindingResult) {
+		if(bindingResult.hasErrors()) {//bindingResult.hasErrors()は検証で一つでもエラーがあればbooleanの値を返す。　エラーがあった場合ture.　
+			ModelAndView modelAndView = new ModelAndView("/board");
+			modelAndView.addObject("commentForm",comment);//モデルに属性を追加する。
+			return modelAndView;
+		}
+		return new ModelAndView("redirect:/board");//return "board"からreturn "redirect:/board";変更して　２重通信を回避。リロードしてもGETメソッドになる。
+	}
+}
